1cdda9a80002733d28a66fc5bd3296a993ee1815
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-07 16:21:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let sub = document.getElementById('create-task-form')
+    
+  sub.addEventListener('submit',function ()  {
+event.preventDefault();
+  let ab = document.getElementById('new-task-description').value
+  let tasks = document.getElementById('tasks')
+  if(ab.trim() > ''){
+    let li = document.createElement('li')
+    let butt = document.createElement('button')
+    butt.setAttribute('class','beb')
+    butt.innerHTML = 'x'
+    li.innerHTML = ab
+    tasks.appendChild(li)
+        li.appendChild(butt)
+        
+
+    reset()
+    dele()
+  }
+  })
+
+function reset() {
+   let ab = document.getElementById('new-task-description').value = null
+}
+function dele(){
+  let beb = document.getElementsByClassName('button')[0]
+  beb.addEventListener('click',function()  {
+    beb.forEach (ele => function(){
+      ele.parentNode.remove()
+    })
+  })
+} 
 });
